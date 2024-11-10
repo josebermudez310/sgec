c4cb7929a3c2c52b3554ba897b75e759a09eb44e
Jose Bermudez <joseber310@gmail.com>
2024-11-10 16:37:47 -0500

    Add graphics page

--- src/app/components/dashboard/dashboar.routes.ts
+++ src/app/components/dashboard/dashboar.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ConfigurationsComponent } from './configurations/configurations.component';
+import { GraphicComponent } from './graphic/graphic.component';
 
 export const routes: Routes = [
     {
@@ -12,6 +13,7 @@ export const routes: Routes = [
             { path: 'home', component: HomeComponent },
             { path: 'profile', component: ProfileComponent },
             { path: 'configurations', component: ConfigurationsComponent },
+            { path: 'graphic', component: GraphicComponent },
             { path: '**', redirectTo: 'home', pathMatch: 'full' }
         ]
     }
