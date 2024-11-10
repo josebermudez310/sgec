33959b7c427d1b31a9244b8a4a7e840f5db2837a
Jose Bermudez <joseber310@gmail.com>
2024-11-10 12:33:02 -0500

    Add profile page

--- src/app/components/dashboard/dashboar.routes.ts
+++ src/app/components/dashboard/dashboar.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
 
 export const routes: Routes = [
     {
@@ -8,6 +9,7 @@ export const routes: Routes = [
         component: DashboardComponent,
         children: [
             { path: 'home', component: HomeComponent },
+            { path: 'profile', component: ProfileComponent },
             { path: '**', redirectTo: 'home', pathMatch: 'full' }
         ]
     }
