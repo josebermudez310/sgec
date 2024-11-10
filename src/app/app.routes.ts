d39dbb8d86ef3c7097bfefc73eeeb5081f586758
Jose Bermudez <joseber310@gmail.com>
2024-11-10 17:49:41 -0500

    Add navigation throw the page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/user/login/login.component';
-import { CalendarComponent } from './components/calendar/calendar.component';
 import { RegisterComponent } from './components/user/register/register.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
-    { path: 'calendar', component: CalendarComponent },
     { path: 'dashboard', loadChildren: () => import('./components/dashboard/dashboard.module').then(m => m.DashboardModule) },
     { path: '**', redirectTo: '/login', pathMatch: 'full' }
 ];
