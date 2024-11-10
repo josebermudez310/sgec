01b0432f1da609645a2b9ce79de8de34e24b58c8
Jose Bermudez <joseber310@gmail.com>
2024-11-10 14:35:47 -0500

    Add users configurations page

--- src/app/components/dashboard/dashboar.routes.ts
+++ src/app/components/dashboard/dashboar.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard.component';
 import { ProfileComponent } from './profile/profile.component';
+import { ConfigurationsComponent } from './configurations/configurations.component';
 
 export const routes: Routes = [
     {
@@ -10,6 +11,7 @@ export const routes: Routes = [
         children: [
             { path: 'home', component: HomeComponent },
             { path: 'profile', component: ProfileComponent },
+            { path: 'configurations', component: ConfigurationsComponent },
             { path: '**', redirectTo: 'home', pathMatch: 'full' }
         ]
     }
