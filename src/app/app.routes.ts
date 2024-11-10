65ba53c5ca5478998d61eaa82387790e59ca00b5
Jose Bermudez <joseber310@gmail.com>
2024-11-10 10:02:04 -0500

    Add dashboard and home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,9 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/user/login/login.component';
 import { CalendarComponent } from './components/calendar/calendar.component';
 
-
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
-    // { path: '**', redirectTo: '/login', pathMatch: 'full' },
     { path: 'calendar', component: CalendarComponent },
-
-    { path: '**', redirectTo: '/calendar', pathMatch: 'full' }
+    { path: 'dashboard', loadChildren: () => import('./components/dashboard/dashboard.module').then(m => m.DashboardModule) },
+    { path: '**', redirectTo: '/login', pathMatch: 'full' }
 ];
